dcc7be40099a294855950ba1230659f3f4552a16
DanilaZhebryakov <64162836+DanilaZhebryakov@users.noreply.github.com>
2022-10-21 12:52:01 +0300

    another func split

--- asm/assembler.cpp
+++ asm/assembler.cpp
@@ -45,6 +45,36 @@ static bool compileInstruction(const char* str, uint8_t** output_ptr, size_t ip,
     return false;
 }
 
+bool placeLabels(label* lbl_place_b, label* lbl_place_e, label* lbl_addr_b, label* lbl_addr_e, uint8_t* addr_origin, bool stop_on_err){
+    bool compilation_error = false;
+    for (label* l_place = lbl_place_b; l_place < lbl_place_e; l_place++){
+        bool found_lbl = false;
+        for (label* l_def = lbl_addr_b; l_def < lbl_addr_e; l_def++){
+            size_t scl = stricmp_len(l_def->name, l_place->name);
+            if (l_def->name[scl] == ':' && (l_place->name[scl] == '\0' || strchr(" +-", l_place->name[scl]))){
+                if(found_lbl){
+                    error_log("Multiple definitions of label %s found\n", l_def->name);
+                    compilation_error = true;
+                    if(stop_on_err){
+                        return false;
+                    }
+                }
+                info_log("add label %s (%X) to constant at %X\n", l_def->name, l_def->addr - addr_origin , l_place->addr - addr_origin);
+                *((PROC_DATA_T*)l_place->addr) += l_def->addr - addr_origin;
+                found_lbl = true;
+            }
+        }
+        if(!found_lbl){
+            compilation_error = true;
+            error_log("Label %s undefined\n", l_place->name);
+        }
+        if (compilation_error && stop_on_err){
+            return false;
+        }
+    }
+    return !compilation_error;
+}
+
 
 uint8_t* asmCompile(const Text input_txt, size_t* size_ptr, bool stop_on_err){
     initLiteralReplaceArray();
@@ -103,32 +133,7 @@ uint8_t* asmCompile(const Text input_txt, size_t* size_ptr, bool stop_on_err){
         return nullptr;
     }
 
-    for (label* l_place = labels; l_place < label_place_ptr; l_place++){
-        bool found_lbl = false;
-        for (label* l_def = labels_last; l_def > label_addr_ptr; l_def--){
-            size_t scl = stricmp_len(l_def->name, l_place->name);
-            if (l_def->name[scl] == ':' && (l_place->name[scl] == '\0' || strchr(" +-", l_place->name[scl]))){
-                if(found_lbl){
-                    error_log("Multiple definitions of label %s found\n", l_def->name);
-                    compilation_error = true;
-                    if(stop_on_err){
-                        break;
-                    }
-                }
-                info_log("add label %s (%X) to constant at %X\n", l_def->name, l_def->addr - out_prog_start , l_place->addr - out_prog_start);
-                *((PROC_DATA_T*)l_place->addr) += l_def->addr - out_prog_start;
-                found_lbl = true;
-            }
-        }
-        if(!found_lbl){
-            compilation_error = true;
-            error_log("Label %s undefined\n", l_place->name);
-        }
-        if (compilation_error && stop_on_err){
-            break;
-        }
-    }
-    if(compilation_error){
+    if(!placeLabels(labels, label_place_ptr, label_addr_ptr + 1, labels_last + 1, out_prog_start, stop_on_err)){
         free(output);
         free(labels);
         error_log("Assembly errors occured. Output not created\n");
